07b463278748d35933224f3cd1a5630194819d57
Olivier Cortès <oc@1flow.io>
2013-07-10 22:20:43 +0200

    detect encoding via bs4 only if not given as constructor argument, and make flake8 happy.

--- strainer/strainer.py
+++ strainer/strainer.py
@@ -7,16 +7,19 @@ Created by: Rui Carmo
 License: MIT (see LICENSE for details)
 """
 
-import os, sys, logging
+import logging
 
 log = logging.getLogger()
 
 from bs4 import BeautifulSoup
 
-import patterns, scorers, cleaners
+import scorers
+import cleaners
+
 
 class Strainer:
-    def __init__(self, prettify = False, add_score = False, block_threshold = 25, article_threshold = 250, parser="html5lib"):
+    def __init__(self, prettify=False, add_score=False, block_threshold=25,
+                 article_threshold=250, parser="html5lib", encoding=None):
         """Setup defaults"""
         assert(parser in ["html5lib", "lxml", "html.parser"])
         self.parser = parser
@@ -24,24 +27,32 @@ class Strainer:
         self.block_threshold = block_threshold
         self.article_threshold = article_threshold
         self.prettify = prettify
+        self.encoding = encoding
 
     def feed(self, buffer):
         """Process buffer and extract significant HTML"""
-        soup = cleaners.set_encoding(
-            cleaners.cleanup(BeautifulSoup(cleaners.remove_breaks(
-                             cleaners.remove_whitespace(buffer)),
-                             self.parser)))
+
+        if self.encoding is None:
+            soup = cleaners.set_encoding(cleaners.cleanup(BeautifulSoup(
+                                        cleaners.remove_breaks(
+                                            cleaners.remove_whitespace(buffer)),
+                                        self.parser)))
+        else:
+            soup = cleaners.cleanup(BeautifulSoup(cleaners.remove_breaks(
+                    cleaners.remove_whitespace(buffer)), self.parser))
+            soup.encode(self.encoding)
+
         aggressive = True
         while True:
-            if aggressive: 
+            if aggressive:
                 soup = cleaners.remove_unlikely(soup)
             soup = cleaners.demote_divs(soup)
             candidates = scorers.text_blocks(soup, self.block_threshold)
             for i in candidates:
-                log.debug(">>> %f: %s" % 
+                log.debug(">>> %f: %s" %
                     (candidates[i]['score'],
                     str(i)[:80].replace('\n',' ')))
-            best_candidate = scorers.highest(candidates)   
+            best_candidate = scorers.highest(candidates)
             if best_candidate:
                 result = scorers.extend(best_candidate, candidates)
             else:
@@ -60,4 +71,4 @@ class Strainer:
                     return clean.contents[0].prettify()
                 else:
                     return clean.contents[0]
-            
+
